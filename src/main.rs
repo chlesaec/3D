864a6b92563c0ca488a21425a704afca7c4e0ccd
chlesaec <christophe.lesaec@gmail.com>
2022-11-11 11:23:40 +0100

    add figures

--- src/main.rs
+++ src/main.rs
@@ -2,53 +2,28 @@ mod expression;
 mod points;
 mod projection;
 
+use std::path::PathBuf;
 use eframe::egui;
 use eframe::egui::{Painter, Context, Id, LayerId, Order};
 use crate::points::{Edge, EdgeAdder, Figure, PointAdder};
 use crate::projection::{Projection, Screen, ToRealScreen};
 use points::Point;
 use eframe::epaint::{ Color32, Stroke };
+use rfd::FileDialog;
+use serde_json;
+use std::fs;
+use serde_json::Value;
 
 struct MyWindowHanlder {
     screens: [Screen; 2],
     figure: Figure<3>
 }
 
-impl MyWindowHanlder {
-    fn build_figure(&mut self) -> &Self {
-      //  self.figure : Figure<'static, 3> = Figure { points: vec![],
-      //      edges: vec![] };
-        self.figure.add_point([-100, -100, -100]);
-        self.figure.add_point( [-100, -100, 100]);
-        self.figure.add_point( [-100, 100, -100]);
-        self.figure.add_point( [-100, 100, 100]);
-        self.figure.add_point( [100, -100, -100]);
-        self.figure.add_point( [100, -100, 100]);
-        self.figure.add_point( [100, 100, -100]);
-        self.figure.add_point( [100, 100, 100]);
-
-        self.figure.add_edge(0,1);
-        self.figure.add_edge(0,2);
-        self.figure.add_edge(0,4);
-        self.figure.add_edge(1,3);
-        self.figure.add_edge(1,5);
-        self.figure.add_edge(2,3);
-        self.figure.add_edge(2,6);
-        self.figure.add_edge(3,7);
-        self.figure.add_edge(4,5);
-        self.figure.add_edge(4,6);
-        self.figure.add_edge(5,7);
-        self.figure.add_edge(6,7);
-        return self;
-    }
-}
-
-
 fn drawEdge(painter: &Painter, s: &Screen, e: &Edge, f: &Figure<3>) {
     let points = f.edge_points(e);
     let p1 = s.project(points.0);
     let p2 = s.project(points.1);
-    painter.line_segment([s.place(&p1),  s.place(&p2)], Stroke::new(3.0, s.color));
+    painter.line_segment([s.place(&p1),  s.place(&p2)], Stroke::new(2.4, s.color));
 }
 
 impl eframe::App for MyWindowHanlder {
@@ -60,8 +35,9 @@ impl eframe::App for MyWindowHanlder {
                 let painter =
                     ctx.layer_painter(LayerId::new(Order::Foreground, Id::new("graphics")));
 
-                let window = self.build_figure();
+                let window = &self;
                 let s1 :&Screen = &window.screens[0];
+
                 window.figure.edges
                     .iter()
                     .for_each(|e: &Edge| -> () {
@@ -70,6 +46,27 @@ impl eframe::App for MyWindowHanlder {
                 let s2 :&Screen = &window.screens[1];
                 window.figure.edges.iter().for_each(|e| -> () {
                     drawEdge(&painter, s2, e, &window.figure);
+                });
+
+                egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+                    // The top panel is often a good place for a menu bar:
+                    egui::menu::bar(ui, |ui| {
+                        ui.menu_button("File", |ui| {
+                            if ui.button("Load").clicked() {
+                                let files: Option<PathBuf> = FileDialog::new()
+                                    .add_filter("figure", &["fig"])
+                                    .set_directory("${HOME}")
+                                    .pick_file();
+                                let path: PathBuf = files.unwrap();
+                                let data = fs::read_to_string(path).expect("Unable to read file");
+                                println!("content = {}", data);
+                                points::loadFigure(data.as_str(), &mut self.figure);
+                            }
+                            if ui.button("Quit").clicked() {
+                                _frame.close();
+                            }
+                        });
+                    });
                 });
             }
         );
@@ -84,14 +81,14 @@ fn main() {
     const s1: Screen = Screen {
         zoom: 150,
         projectionpos: -250,
-        center: Point { coords: [-60, 0] },
+        center: Point { coords: [-30, 0] },
         color: Color32::RED,
         size: [900, 700],
     };
     const s2: Screen = Screen {
         zoom: 150,
         projectionpos: -250,
-        center: Point { coords: [60, 0] },
+        center: Point { coords: [30, 0] },
         color: Color32::BLUE,
         size: [900, 700],
     };
--- src/points.rs
+++ src/points.rs
@@ -1,4 +1,4 @@
-
+use serde_json::Value;
 
 //#[derive(Clone, Copy)]
 pub struct Point<const N: usize> {
@@ -46,6 +46,108 @@ impl<const N: usize> EdgeAdder for Figure<N> {
     }
 }
 
+pub struct Error {
+    pub cause: String
+}
+
+pub fn loadFigure(json_source: &str, figure:&mut Figure<3>) {
+
+    fn toPoint(points: &Value) -> Result<[i64; 3], Error> { // move Option to Result
+        match points {
+            Value::Array(x) => if x.len() == 3 {
+                let x0: &Value = &x[0];
+                let x1: &Value = &x[1];
+                let x2: &Value = &x[2];
+                match (x0, x1, x2) {
+                    (Value::Number(v1),
+                        Value::Number(v2),
+                        Value::Number(v3)) =>
+                        match (v1.as_i64(),
+                               v2.as_i64(),
+                               v3.as_i64()) {
+                            (Some(i1), Some(i2), Some(i3)) => Result::Ok([i1, i2, i3]),
+                            _ => Result::Err(Error { cause: "Array size error".to_string() })
+                        }
+
+                    (_,_,_) => Result::Err(Error { cause: "Array size error".to_string() })
+                }
+            }
+            else {
+                Result::Err(Error { cause: "Array size error".to_string() })
+            },
+            _ => Result::Err(Error { cause: "Not an array".to_string() })
+        }
+    }
+    fn loadPoints(figure:&mut Figure<3>, points: &Value) {
+        match points {
+            Value::Array(content) => {
+                content.iter()
+                    .map(|x| toPoint(x))
+                    .for_each(|p | {
+                        let fx = match p {
+                            Result::Ok(x) => figure.add_point(x),
+                            Result::Err(e) => {
+                                panic!("{}", e.cause);
+                            },
+                        };
+                    });
+            },
+            _ => panic!("points not an array")
+        }
+    }
+    fn loadEdges(figure:&mut Figure<3>, edges: &Value) {
+        match edges {
+            Value::Array(content) => {
+                content.iter()
+                    .for_each(|edge | {
+                        match edge {
+                            Value::Array(content) => {
+                                if content.len() != 2 {
+                                    panic!("edge should have 2 ends")
+                                }
+                                let p1 = &content[0];
+                                let p2 = &content[1];
+                                match (p1, p2) {
+                                    (Value::Number(point1), Value::Number(point2)) => {
+                                        match (point1.as_u64(), point2.as_u64()) {
+                                            (Some(u1), Some(u2)) => {
+                                                figure.add_edge(u1 as usize, u2 as usize);
+                                            },
+                                            _ => panic!("end should be number")
+                                        }
+                                    },
+                                    _ => panic!("edge ends should be number")
+                                }
+                            },
+                            _ => panic!("edges not an array")
+                        }
+                    });
+            },
+            _ => panic!("edges not an array")
+        }
+    }
+    figure.points.clear();
+    figure.edges.clear();
+    let json_figure = serde_json::from_str::<Value>(&json_source).unwrap();
+    match json_figure {
+        Value::Object(x) => {
+            let points = x.get("points");
+            match points {
+                Some(pts) => loadPoints(figure, pts),
+                None => panic!("not an object")
+            }
+
+            let edges = x.get("edges");
+            match edges {
+                Some(pts) => loadEdges(figure, pts),
+                None => panic!("not an object")
+            }
+            figure.edges.iter().for_each(|e|  println!("edge : {} to {}", e.points.0, e.points.1));
+        }
+        _ => panic!("not an object")
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -67,24 +169,28 @@ mod tests {
         assert_eq!(3, p1.coords[0]);
         assert_eq!(11, p2.coords[2]);
     }
+
+    #[test]
+    fn loadTest() {
+        let figureDesc = "{
+                \"points\": [
+                   [10, 20, 10],
+                   [-10, 0, -10],
+                   [-10, 20, 0]
+                ],
+             \"edges\": [
+                  [0,1], [1,2], [2,0]
+            ]
+        }";
+        let mut fig: Figure<3> = Figure {
+            points: vec![],
+            edges: vec![]
+        };
+        loadFigure(&figureDesc, &mut fig);
+        assert_eq!(3, fig.points.len());
+        assert_eq!(3, fig.edges.len());
+    }
 }
 pub trait Distance<T, const N: usize> {
     fn calc(from: T) -> f64;
 }
-/*
-impl<'a, const N: usize> Distance<Point<N>, N> for Point<N> {
-    fn calc(from: &'a Point<N>) -> f64 {
-        let mut x: f64 = 0.0;
-        for i in 0..N {
-            x += from.coords[i].pow(2) as f64;
-        }
-        x.sqrt()
-    }
-}*/
-/*
-impl<const N: usize> Distance<Edge<N>, N> for Point<N> {
-    fn calc(from: Edge<N>) -> f64 {
-        0.0
-    }
-}
-*/
